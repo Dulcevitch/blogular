770c38020951e4d4f129c53046cb38d1e6226be3
ludovic.dembski@gmail.com <ludovic.dembski@gmail.com>
2019-05-03 13:26:53 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'model/Post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-super-blog';
+export class AppComponent implements OnInit {
+  title = 'Blogular';
+
+  posts: any[] = [
+    new Post('Premier titre', 'Ici le contenu du premier post.'),
+    new Post('Deuxi\u00e8me titre', 'Ici le contenu du deuxi\u00e8me post.'),
+    new Post('Troisi\u00e8me titre', 'Ici le contenu du troisi\u00e8me post.'),
+  ];
+
+  ngOnInit() {
+
+  }
 }
